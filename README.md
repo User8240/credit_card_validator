51d6193a53f94caf0ac59e301f3356780652dc70
Grace Kostanich <fl5812597@gmail.com>
2022-06-10 02:25:07 -0700

    WIP: Add additional features to multiply every other card value by 2, and begin to separate double digit values from array
    Co-authored by: Daniel Ware <waredanielb@gmail.com>

--- README.md
+++ README.md
@@ -33,18 +33,42 @@ Copyright (c) _5/29/2022_ _Grace Kostanich & Daniel Ware_
 
 **Describe:** _cardNumberCutter()_
 
-**Test:** _"It will remove all spacing between numbers"_
+**Test:** _"It will remove all spacing between numbers in a string"_
 
-Code: _cardNumberCutter("4102 0808 8043 5620");_
+Code: _stringWithSpaces("4102 0808 8043 5620");_
 
 Expected Output: _("4102080880435620")_
 
 
 **Test:** _"It will separate a 16 digit card number into individual strings"_
 
-Code: _cardNumberCutter("4102080880435620");_
+Code: _stringWithSpaces("4102080880435620");_
 
-Expected Output: _"away"_
+Expected Output: _("4", "1", "0", "2", "0", "8", "0", "8", "8", "0", "4", "3", "5", "6", "2", "0")_
+
+
+**Test:** _"It will run .split() which will convert the string into an array and place each number at a separate index"_
+
+Code: _stringWithSpaces("4102080880435620");_
+
+Expected Output: _["4", "1", "0", "2", "0", "8", "0", "8", "8", "0", "4", "3", "5", "6", "2", "0"]_
+
+
+**Test:** _"It will have a for loop that starts at index one and increments to every other value"_
+
+Code: _stringWithSpaces("4102080880435620");_
+
+Expected Output: _["1", "2", "8", "8", "0", "3", "6", "0"]_
+
+
+**Test:** _"It will double every value in our new array that contained every other value of the original credit card number "_
+
+Code: _stringWithSpaces("4102080880435620");_
+
+Expected Output: _["2", "4", "16", "16", "0", "6", "12", "0"]_
+
+----------------------------------------------------------------------
+
 
 
 **Test:** _"It will add 'way' to the end of words that begin with an array of vowels."_
--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,55 @@
 //Business Logic
 
-function numberOfOccurrencesInText(text) {
-  if (text.trim().length === 0) {
-    return 0;
-//  
-function numberOfOccurrencesInText(text) {
-  cardNumber= text.replaceAll(" ", "")
+//The code below is what we had before the edits Grace made 
+
+ function stringWithSpaces(removeSpaces) {
+   stringWithoutSpaces = removeSpaces.replaceAll(" ", "");
+    const creditcardArray  = stringWithoutSpaces.split("");
+    let newArray = [];
+    for (let i=1; i < creditcardArray.length; i += 2) {
+    newArray.push(creditcardArray[i]); 
+   }
+   const multipliedArray = newArray.map(function(element) {
+     return element * 2;
+   });
+   
+   console.log(multipliedArray);
+ }
+ stringWithSpaces("4102 0808 8043 5620");
 //
-}
 
+ function stringWithSpaces(removeSpaces) {
+   stringWithoutSpaces = removeSpaces.replaceAll(" ", "");
+    const creditcardArray  = stringWithoutSpaces.split("");
+    let newArray = [];
+    for (let i=1; i < creditcardArray.length; i += 2) {
+    newArray.push(creditcardArray[i]); 
+   }
+   const multipliedArray = newArray.map(function(element) {
+     return element * 2;
+   });
+   let digitArray = multipliedArray.map(function(element) {
+      if (element > 9) {
+        console.log(element);
+        test = element.toString().split("")
+        return test.join(" ")
+      } 
+      else (element <= 9); {
+         return element = element;
+      }
+     });
 
-function creditcardstringwithspaces(removeSpaces) {
-  removeSpaces.replaceAll(" ", "");
-  }
+     //BELOW: if a digit is not a number but a string, we want to separate
+     //the strings into two separate strings somehow, define them as 
+     //values with .val() somehow, then add the string together =>
 
-removeSpaces("4102 0808 8043 5620")
-
-function stringMaker(creditcardstringwithoutspaces) {
-
- 
-   const creditcardArray  = creditcardstringwithoutspaces.split("");
-   let newArray = [];
-   for (let i=1; i < creditcardArray.length; i += 2) {
-   newArray.push(creditcardArray[i]); 
-  }
-  console.log(newArray);
-}
-
-stringMaker("4102080880435620");
+     //if (digitArray === NaN ) {
+     //  digitArray = digitArray.join(",");
+     //  digitArray = digitArray.split(" ");
+     //  return digitArray
+     
+   console.log(digitArray);
+    }
+ stringWithSpaces("4102 0808 8043 5620");
 
 //UI Logic
